d14fb0bd11645beef3f94cf537ebab2a82428fc6
Oliver Marquez <marquez3663@gmail.com>
2021-07-25 10:42:51 -0400

    Explicacion de los tipos de datos y el tipado de estos con typescript

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,31 @@
 "use strict";
 console.log('Hola Typescript con Platzi!');
+//Number
+//Explicito
+var phone;
+phone = 1;
+phone = 54234567;
+// phone = 'Hola'; // Error
+//Inferido
+var phoneNumber = 54234567;
+phoneNumber = 123;
+// phoneNumber = true; // Error de tipo de dato
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+//Tipo Boolean
+//Tipado explicito
+var isPro;
+isPro = true;
+//Inferido
+var isUserPro = false;
+isUserPro = true;
+//Strings
+//Tipado explicito
+var username = 'marquez3663';
+username = 'Oliver';
+//Template String
+// Uso de back-tick "`"
+var userInfo;
+userInfo = "\n    User Info:\n    username: " + username + "\n    firstName: " + (username + ' Marquez') + "\n    phone: " + phone + "\n    isPro: " + isPro + "\n\n";
+console.log('userInfo', userInfo);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,44 @@
 console.log('Hola Typescript con Platzi!');
+
+//Number
+//Explicito
+let phone: number;
+phone = 1;
+phone = 54234567;
+// phone = 'Hola'; // Error
+
+//Inferido
+let phoneNumber = 54234567;
+phoneNumber = 123;
+// phoneNumber = true; // Error de tipo de dato
+
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+//Tipo Boolean
+//Tipado explicito
+let isPro: boolean;
+isPro = true;
+
+//Inferido
+let isUserPro = false;
+isUserPro = true;
+
+//Strings
+//Tipado explicito
+let username: string = 'marquez3663';
+username = 'Oliver';
+
+//Template String
+// Uso de back-tick "`"
+let userInfo: string;
+userInfo= `
+    User Info:
+    username: ${username}
+    firstName: ${username + ' Marquez'}
+    phone: ${phone}
+    isPro: ${isPro}
+
+`;
+console.log('userInfo', userInfo);
