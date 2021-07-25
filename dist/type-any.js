1de7f71223f39e25b220f7c751e728a6c0db5128
Oliver Marquez <marquez3663@gmail.com>
2021-07-25 11:02:43 -0400

    Explicacion de el valor (any), ejemplos de esta. es una mala practica

---
+++ dist/type-any.js
@@ -0,0 +1,16 @@
+"use strict";
+// Tipo explicito
+var idUser;
+idUser = 1;
+idUser = 1; //Number
+idUser = "1"; //String
+console.log('idUser', idUser);
+// Tipo inferido 
+var otherId;
+otherId = 1;
+otherId = '1';
+console.log('otherId', otherId);
+var surprise = 'hello typescript';
+// surprise.sayHello();//Error
+var res = surprise.substring(6); // Error
+console.log('res', res);
---
+++ src/type-any.ts
@@ -0,0 +1,17 @@
+// Tipo explicito
+let idUser: any;
+idUser = 1;
+idUser = 1; //Number
+idUser = "1"; //String
+console.log('idUser', idUser);
+
+// Tipo inferido 
+let otherId;
+otherId = 1;
+otherId = '1';
+console.log('otherId', otherId);
+
+let surprise: any = 'hello typescript';
+// surprise.sayHello();//Error
+const res = surprise.substring(6);// Error
+console.log('res', res);
